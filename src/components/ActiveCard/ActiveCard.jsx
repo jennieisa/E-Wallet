043c6f23b51a4f8e1c6391f21257841b9356786d
jennieisa <jennieisakssoon97@gmail.com>
2022-09-18 18:39:00 +0200

    added jsx to the active card component

---
+++ src/components/ActiveCard/ActiveCard.jsx
@@ -0,0 +1,28 @@
+const ActiveCard = () => {
+
+    return (
+        <section>
+            <p>ACTIVE CARD</p>
+            <div>
+                <section>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section>
+                    <img src="" alt="" />
+                </section>
+                <p>123 54678 9101 1123</p>
+                <section>
+                    <p>CARDHOLDER NAME</p>
+                    <p>VALID THRU</p>
+                </section>
+                <section>
+                    <p>NAMN NAMNSSON</p>
+                    <p>12/22</p>
+                </section>
+            </div>
+        </section>
+    )
+}
+
+export default ActiveCard;
--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -1,4 +1,6 @@
 import Header from '../components/Header/Header';
+import ActiveCard from '../components/ActiveCard/ActiveCard';
+
 
 const CardsPage = () => {
     return (
@@ -7,6 +9,7 @@ const CardsPage = () => {
             <div>
                 <h2>kort</h2>
             </div>
+            <ActiveCard />
         </div>
     )
 }
